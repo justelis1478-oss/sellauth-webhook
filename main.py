deb0122166c239d8930d0c2b46b897b1518fd47e
justelis1478-oss <justelis1478@gmail.com>
2026-03-19 16:14:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
+
+FEEDBACK_WEBHOOK = os.getenv("https://discord.com/api/webhooks/1484192884214468810/sim7ykN4Cj9F3xXsSZflIkB_nQbFMxgaXo30vQocL891dN6EWRub4B_xJHd0hYn4DOXb")
+RESTOCK_WEBHOOK = os.getenv("https://discord.com/api/webhooks/1483873834024304772/T9bbCviBm_sJRZH3NMpYGBSQ3ErN5V7X_j1SYaAUCfQEHqLwB0gCpXA7yL8EqEB3UBFh")
 
 @app.route("/")
 def home():
     return "OK", 200
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
+# 🔹 FEEDBACK
+@app.route("/feedback", methods=["POST"])
+def feedback():
+    data = request.json
+
+    message = {
+        "content": f"📝 New feedback:\n{data}"
+    }
+
+    requests.post(FEEDBACK_WEBHOOK, json=message)
+    return "OK", 200
+
+# 🔹 RESTOCK
+@app.route("/restock", methods=["POST"])
+def restock():
+    data = request.json
+
+    message = {
+        "content": f"📦 Restock:\n{data}"
+    }
+
+    requests.post(RESTOCK_WEBHOOK, json=message)
     return "OK", 200
